3bb49841011d3bf003b968630fc79fa8c052d683
Dorothy Kucar <dorothykucar@dorothys-mbp.lan>
2024-01-19 17:26:57 -0700

    rename log4j to log4j2, added another test case

--- src/main/scala/CompareRunner.scala
+++ src/main/scala/CompareRunner.scala
@@ -1,6 +1,5 @@
-import SparkUtilities.{compareDFs, flattenDataFrame}
+import SparkUtilities.{compareDFsSymDiff, flattenDataFrame}
 import org.apache.log4j.Logger
-import org.apache.log4j.Level
 import org.apache.spark.sql.SparkSession
 
 object CompareRunner {
@@ -13,17 +12,19 @@ object CompareRunner {
   val USER_HOME_DIR = System.getProperty("user.home")
 
   def main(args: Array[String]): Unit = {
+    spark.sparkContext.setLogLevel("WARN")
     val logger = Logger.getLogger("")
-    Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
-    Logger.getLogger("org.apache").setLevel(Level.OFF)
-    Logger.getLogger("org").setLevel(Level.OFF)
     val rootPath = s"$USER_HOME_DIR/Repos/my-spark-utilities/src/main/resources"
     val fileName1 = "sample.json"
-    val fileName2 = "sample.json"
+    val fileName2 = "sample2.json"
     val df1 = spark.read.option("multiline", "true").json(s"$rootPath/$fileName1")
     val df2 = spark.read.option("multiline", "true").json(s"$rootPath/$fileName2")
 
-    val result = compareDFs(flattenDataFrame(df1, true), flattenDataFrame(df2, true))
-    logger.info(s"dataframe1 is equal to dataframe2: $result")
+    val resultFalse = compareDFsSymDiff(flattenDataFrame(df1, true), flattenDataFrame(df2, true))
+    logger.warn(s"dataframe1 ($fileName1) is equal to dataframe2 ($fileName2): $resultFalse")
+
+    val resultTrue = compareDFsSymDiff(flattenDataFrame(df1, true), flattenDataFrame(df1, true))
+    logger.warn(s"dataframe1 ($fileName1) is equal to dataframe2 ($fileName1): $resultTrue")
+
   }
 }
